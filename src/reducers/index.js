0fdd7cc34ba0d0e273926ec8409ed4965f874e8c
Allen <aestes07@gmail.com>
2018-01-28 15:11:47 -0600

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import Products from './products-reducer';
 
 const allReducers = combineReducers({
-
+  products: Products,
 });
 
 export default allReducers;
